9e79649666f4979d72122702982e0649eb6b2e73
Bryan T <blondejamtart@gmail.com>
2024-07-08 09:49:31 +0100

    fix guess_ssl_location for windows

--- utilfuncs.py
+++ utilfuncs.py
@@ -391,25 +391,25 @@ def guess_ssl_location(conf):
 
     set_env_verbose(conf, 'INCLUDES_SSL', match_path(
         conf,
-        [
+        os.path.join(
             '{options.ssl}/build/{options.dest_platform}/include',
             '{options.ssl}/include',
             'dependencies/{options.dest_platform}/libressl/include',
             'dependencies/{options.dest_platform}/staging/usr/include',
             f'{sysroot}/usr/include',
-        ],
+        ),
         message='Specify --ssl')
     )
     set_env_verbose(conf, 'STLIBPATH_SSL', match_path(
         conf,
-        [
+        os.path.join(
             '{options.ssl}/build/{options.dest_platform}/lib',
             '{options.ssl}/lib',
             '{options.ssl}/ssl',
             'dependencies/{options.dest_platform}/libressl/lib',
             'dependencies/{options.dest_platform}/staging/usr/lib', 
             f'{sysroot}/usr/lib',           
-        ],
+        ),
         message='Specify --ssl')
     )
     conf.env.STLIB_SSL = ['ssl', 'crypto']
